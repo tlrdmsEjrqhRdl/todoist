cace2f55598554c0bf800a8e7becd248968fc8d9
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-09 21:45:52 +0900

    a

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="bg-white w-screen h-screen flex items-center justify-center">
-      <p className="text-black text-xl">로딩중</p>
+      <img src="./public/todoist.png" alt="tlqkf" />
     </div>
   );
 }
