aa719e4028566db34c1a4974112493f948f20ffc
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-11 00:25:02 +0900

    이미지는 불러와짐

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="bg-white w-screen h-screen flex items-center justify-center">
-      <img src="./public/todoist.png" alt="tlqkf" />
+    <div className="bg-white fixed inset-0 flex items-center justify-center">
+      <img src="/todoist.png" alt="tlqkf" />
     </div>
   );
 }
--- app/pages/Login.tsx
+++ app/pages/Login.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Login() {
   return (
     <>
-      <div className='bg-white w-full h-full'>
-        <p className=' text-xl'>안녕</p>
+      <div className='bg-white w-full h-full flex justify-center items-center'>
+        <img src='/todoist.png' height={40} width={40}></img>
       </div>
     </>
   )
