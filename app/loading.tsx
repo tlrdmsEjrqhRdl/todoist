76e715e625c5ac2075fe1a41949a44983ff8dcad
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-12 10:46:04 +0900

    a

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="bg-white fixed inset-0 flex items-center justify-center">
-      <img src="/todoist.png" alt="tlqkf" />
+      <img src="/todoist.png" alt="failed loading"  className=" w-5 h-5"/>
     </div>
   );
 }
