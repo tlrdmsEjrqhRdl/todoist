6956eb584eef3c74b39d6f4010fd826e0262de19
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-14 14:06:27 +0900

    Loading 개편

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,15 @@
 export default function Loading() {
   return (
-    <div className="bg-white fixed inset-0 flex items-center justify-center">
-      <img src="/todoist.png" alt="failed loading"  className=" w-5 h-5"/>
+    <div
+      className="bg-white fixed inset-0 flex items-center justify-center"
+      aria-label="Loading"
+      role="status"
+    >
+      <img
+        src="/todoist.png"
+        alt="Loading spinner"
+        className="w-5 h-5 animate-spin"
+      />
     </div>
   );
 }
