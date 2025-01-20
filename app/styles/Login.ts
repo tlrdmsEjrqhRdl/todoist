f284f6fd62cf51ebbe32f1766aceb1c2b62d9eb6
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-21 01:22:06 +0900

    자기전 커밋

--- app/styles/Login.ts
+++ app/styles/Login.ts
@@ -1,1 +1,7 @@
-import styles from styled-components
+import styled from 'styled-components';
+
+export const MainDiv = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: white;
+`
